49e302b8c13ca386912101045ea8fd8a312ebc38
Alejandro <aobregon00445@ufide.ac.cr>
2026-03-25 13:43:18 -0600

    Api Con Seguridad

--- Vehiculo.API/API/Program.cs
+++ Vehiculo.API/API/Program.cs
@@ -1,4 +1,4 @@
-using Abstracciones.Interfaces.DA;
+﻿using Abstracciones.Interfaces.DA;
 using Abstracciones.Interfaces.Flujo;
 using Abstracciones.Interfaces.Reglas;
 using Abstracciones.Interfaces.Servicios;
@@ -8,8 +8,29 @@ using DA.Repositorios;
 using Flujo;
 using Reglas;
 using Servicios;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Autorizacion.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var tokenConfig = builder.Configuration.GetSection("Token").Get<TokenConfiguracion>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = tokenConfig.Issuer,
+            ValidAudience = tokenConfig.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                                           Encoding.UTF8.GetBytes(tokenConfig.key))
+        };
+    });
 
 // Add services to the container.
 
@@ -32,6 +53,14 @@ builder.Services.AddScoped<IConfiguracion, Configuracion>();
 builder.Services.AddScoped<IRegistroReglas, RegistroReglas>();
 builder.Services.AddScoped<IRevisionReglas, RevisionReglas>();
 
+// ★ Registrar servicios del paquete de Autorización
+builder.Services.AddTransient<Autorizacion.Abstracciones.Flujo.IAutorizacionFlujo,
+                               Autorizacion.Flujo.AutorizacionFlujo>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.ISeguridadDA,
+                               Autorizacion.DA.SeguridadDA>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.IRepositorioDapper,
+                               Autorizacion.DA.Repositorios.RepositorioDapper>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,6 +72,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.AutorizacionClaims();
 app.UseAuthorization();
 
 app.MapControllers();
